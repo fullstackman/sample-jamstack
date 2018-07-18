a0fa7bc1c2ba5d293c772b6aaddb842da4b04481
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-07-18 13:52:26 -0400

    Falling back to a Unicode 6.0 emoji

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFoundPage = () => (
   <div style={{textAlign:"center"}}>
     <p style={{fontSize:"2.3em"}}>RESOURCE NOT FOUND</p>
-  	<p style={{fontSize:"200px"}}>🤷🏾</p>
+  	<p style={{fontSize:"200px"}}>🙅</p>
     <p style={{fontSize:"1.3em"}}>Maybe you typed something wrong? Or you just clicked on a bad link.</p>
     <p style={{display:"none"}}>If you're a hacker, just turn back now. =P </p>
   </div>
