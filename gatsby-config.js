76d1a63d297ba15dd9b653c335a0780f43fa409b
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-07-19 09:28:42 -0400

    Added reporting uri headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,8 +39,8 @@ module.exports = {
       options: {
         /*This affects every page that we actually build*/
         allPageHeaders: [
-          "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self'; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
-          /*"Link: </static/my-logo.png>; rel=preload; as=image",*/
+          "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self'; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; report-uri https://aagamamusic.report-uri.com/r/d/csp/enforce;",
+          "Link: </static/logo.png>; rel=preload; as=image",
         ],
         headers: {
           /*This afffects every page requested, even malicious requests and 404s*/
@@ -50,14 +50,12 @@ module.exports = {
             "X-Content-Type-Options: nosniff",
             "Referrer-Policy: strict-origin-when-cross-origin",
             "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
-            "Content-Security-Policy: default-src 'none'",
-            /*"Expect-CT: max-age=86400, enforce, report-uri='#'"
-            report-uri https://endpoint.com*/
+            "Content-Security-Policy: default-src 'none'; report-uri https://aagamamusic.report-uri.com/r/d/csp/enforce",
+            "Expect-CT: max-age=86400, enforce, report-uri='https://aagamamusic.report-uri.com/r/d/ct/enforce'"
             "Feature-Policy: vibrate 'none';  microphone 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'self'; fullscreen 'self'; payment 'self'; usermedia 'self'; sync-xhr 'self'; geolocation 'self'; midi 'self'; notifications 'self'; push 'self';",
             "A-Music-Hacker: '(¬‿¬) If you are reading this, send a message to someone[at]example[dot]com and mention this header.'",
           ],
           "/admin/*": [
-            "Content-Security-Policy: default-src: 'self'",
             "Cache-Control: must-revalidate, no-cache, no-store, pre-check=0, post-check=0, max-age=0, s-maxage=0",
             "Expires: 0",
             "Pragma: no-cache",
