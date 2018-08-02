8c015ac93be3d93d0dd35f6242215ff2e723729b
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-08-02 10:27:45 -0400

    Switching CSP over to report mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       options: {
         /*This affects every page that we actually build*/
         allPageHeaders: [
-          "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self'; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; report-uri https://aagamamusic.report-uri.com/r/d/csp/enforce;",
+          "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self'; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; report-uri https://aagamamusic.report-uri.com/r/d/csp/report;",
         ],
         headers: {
           /*This afffects every page requested, even malicious requests and 404s*/
@@ -49,15 +49,15 @@ module.exports = {
             "X-Content-Type-Options: nosniff",
             "Referrer-Policy: strict-origin-when-cross-origin",
             "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
-            "Content-Security-Policy: default-src 'none'; report-uri https://aagamamusic.report-uri.com/r/d/csp/enforce",
-            "Expect-CT: max-age=86400, enforce, report-uri='https://aagamamusic.report-uri.com/r/d/ct/enforce'",
+            "Content-Security-Policy: default-src 'none'; report-uri https://aagamamusic.report-uri.com/r/d/csp/report",
+            "Expect-CT: max-age=86400, enforce, report-uri='https://aagamamusic.report-uri.com/r/d/ct/report'",
             "Feature-Policy: vibrate 'none';  microphone 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'self'; fullscreen 'self'; payment 'self'; usermedia 'self'; sync-xhr 'self'; geolocation 'self'; midi 'self'; notifications 'self'; push 'self';",
             "A-Music-Hacker: '(¬‿¬) If you are reading this, send a message to someone[at]example[dot]com and mention this header.'",
           ],
           /*Just the Netlify CMS Admin panels*/
           "/admin/*": [
             "Cache-Control: must-revalidate, no-cache, no-store, pre-check=0, post-check=0, max-age=0, s-maxage=0",
-            "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self' https://raw.githubusercontent.com; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; report-uri https://aagamamusic.report-uri.com/r/d/csp/enforce;",
+            "Content-Security-Policy: default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com; connect-src 'self'; img-src 'self' https://raw.githubusercontent.com; media-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; report-uri https://aagamamusic.report-uri.com/r/d/csp/report;",
             "Expires: 0",
             "Pragma: no-cache",
             "X-Robots-Tag: noindex,nofollow",
