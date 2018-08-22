498b6e44f0ee91d5d304aa1c6677742db07af652
Brandon Diaz-Abreu <yuey@aagamamusic.com>
2018-08-22 12:18:25 -0400

    Figured out how to store user journey in react state

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,46 +9,63 @@ import './all.sass'
 const brandName="Mangu";
 const thisYear=(new Date()).getFullYear();
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-	<Helmet>
-	    <title>{brandName} | A GatsbyJS+Netlify Demo Site</title>
-		<meta name="description" content="Mangu is this beautiful demo web site."/>
-		<meta name="keywords" content="demo, gatsbyjs, netlify"/>
-		<meta name="image" content="https://mangu.netlify.com/img/logo.png" />
-	    {/*💽 aagama music is an independent record label that is empowering artists to create and share music in a way that empowers fans and other artists to participate in the independent music revolution! ✊*/}
-		{/*Yuey, yueyonthebeat, aagama, aagamamusic, agm records, independent record label, free beats, creative commons music*/}
-		{/* Schema.org tags */}
-        <script type="application/ld+json">
-          {JSON.stringify(config.schemaObject)}
-        </script>
-		<meta name="theme-color" content="#ff4400"/>
-		<meta name="twitter:card" content="summary_large_image" />
-		<meta name="twitter:site" content="@fullstackman" />
-		<meta name="twitter:creator" content="@fullstackman" />
-		<meta property="og:url" content="https://mangu.netlify.com/" />
-		<meta property="og:title" content="Mangu | A GatsbyJS+Netlify Demo Site" />
-		<meta property="og:description" content="Mangu is this beautiful demo web site." />
-		<meta property="og:image" content="https://mangu.netlify.com/img/logo.png" />
-		<link rel="manifest" href="site.webmanifest"/>
-		{/* Place favicon.ico in the root directory*/}
-		<link rel="apple-touch-icon" href="/img/logo.png"/>
-	</Helmet>
-    <Navbar />
-    <div>{children()}</div>
-	<div className="site-footer">
-	    <ul>
-	    <li>&#169; {brandName} {thisYear}</li>
-	      <li><a>Privcay Policy</a></li>
-	      <li><a>Terms</a></li>
-	      <li><a>Box of chocolates</a></li>
-	    </ul>
-  </div>
-  </div>
-)
+export default class Template extends React.Component {
+  constructor(props) {
+    super(props);
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    // current state of the pages the user has visited in this session
+    this.state = {
+      userJourney: props.location.pathname,
+    }
+
+    // event listener to update the userJourney variable
+    this.props.history.listen((location, action) => {
+      //console.log(`${location.pathname} -- Route change is happening!`);
+      this.setState((prevState) => {
+        // Important: read `prevState` instead of `this.state` when updating.
+        return {userJourney: `${prevState.userJourney} --> ${location.pathname}`}
+      });
+    });
+  }
+  render() {
+    let zPath = this.state.userJourney;
+    return(
+    <div>
+      <Helmet>
+        <title>{brandName} | A GatsbyJS+Netlify Demo Site</title>
+        <meta name="description" content="Mangu is this beautiful demo web site."/>
+        <meta name="keywords" content="demo, gatsbyjs, netlify"/>
+        <meta name="image" content="https://mangu.netlify.com/img/logo.png" />
+          {/*💽 aagama music is an independent record label that is empowering artists to create and share music in a way that empowers fans and other artists to participate in the independent music revolution! ✊*/}
+        {/*Yuey, yueyonthebeat, aagama, aagamamusic, agm records, independent record label, free beats, creative commons music*/}
+        {/* Schema.org tags */}
+            <script type="application/ld+json">
+              {JSON.stringify(config.schemaObject)}
+            </script>
+        <meta name="theme-color" content="#ff4400"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@fullstackman" />
+        <meta name="twitter:creator" content="@fullstackman" />
+        <meta property="og:url" content="https://mangu.netlify.com/" />
+        <meta property="og:title" content="Mangu | A GatsbyJS+Netlify Demo Site" />
+        <meta property="og:description" content="Mangu is this beautiful demo web site." />
+        <meta property="og:image" content="https://mangu.netlify.com/img/logo.png" />
+        <link rel="manifest" href="site.webmanifest"/>
+        {/* Place favicon.ico in the root directory*/}
+        <link rel="apple-touch-icon" href="/img/logo.png"/>
+      </Helmet>
+      {/* for debugging: <div>userJourney is now: {zPath}</div>*/}
+      <Navbar />
+      <div>{this.props.children()}</div>
+      <div className="site-footer">
+        <ul>
+          <li>&#169; {brandName} {thisYear}</li>
+          <li><a>Privcay Policy</a></li>
+          <li><a>Terms</a></li>
+          <li><a>Box of chocolates</a></li>
+        </ul>
+      </div>
+    </div>
+    )
+  }
 }
-
-export default TemplateWrapper
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <div>
-        <section class="hero home-hero is-fullheight">
-          <div class="hero-body home-body">
-            <div class="container">
-              <h1 class="title">A Beautiful Website</h1>
-              <h2 class="subtitle">This is the future! (powered by Bulma CSS)</h2>
+        <section className="hero home-hero is-fullheight">
+          <div className="hero-body home-body">
+            <div className="container">
+              <h1 className="title">A Beautiful Website</h1>
+              <h2 className="subtitle">This is the future! (powered by Bulma CSS)</h2>
               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula sem vel enim tincidunt pulvinar. Proin vulputate augue ligula, mattis sagittis enim facilisis vitae. Vivamus pretium augue id magna efficitur, id blandit neque ullamcorper. Aenean viverra bibendum suscipit. Nam vehicula ligula at neque tristique rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus ex velit, ultricies id fermentum et, convallis in nisl. Ut elementum leo vitae mattis faucibus. Maecenas quis eros gravida, mollis lacus porttitor, sollicitudin lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed mattis vestibulum leo, ac varius dui. Sed volutpat enim ut libero tristique venenatis.
                 <br />Sed vehicula, orci quis tristique venenatis, libero elit viverra tellus, vitae pellentesque nisl neque vitae leo. Morbi ac diam ullamcorper, vehicula eros sed, aliquam nunc. Integer consectetur luctus velit, tristique posuere felis euismod vitae. Vivamus sit amet rutrum velit, id euismod elit. Phasellus a augue justo. Integer accumsan placerat purus eu gravida. Donec facilisis pretium condimentum. Donec tempor elit metus, facilisis mattis enim ultrices sed. Fusce non nulla a nisi blandit efficitur. Vivamus lobortis ipsum malesuada, gravida sem sed, malesuada purus. Praesent tristique feugiat nisl quis sagittis. Mauris malesuada interdum urna vel sagittis. Cras eu finibus ipsum, vitae sagittis nulla. Aenean auctor ut leo vel consectetur. Proin tincidunt diam eget nibh fringilla, vel dignissim felis fermentum. </p>
             </div>
